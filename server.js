e315e7a82e3b9aa8b4720dcd7f7ce18c38c1cea2
Rafal <awrug3@gmail.com>
2018-10-10 20:40:19 +0200

    'Fix'

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-app.use(express.static(`${__dirname}/dist/`));
+app.use(express.static(`${__dirname}/dist`));
 app.get(/.*/, (req, res) => {
   res.sendfile(`${__dirname}/dist/index.html`);
 });
+
 app.listen(port);
 
